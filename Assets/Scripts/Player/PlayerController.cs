247d22286cf7818ad0751199ca6487592b3b04f1
Neslihan Çetinkaya <cetinkayaneslihan98@gmail.com>
2023-02-27 01:16:24 +0300

    Ingress Movement added, DoTween and Music Pack added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using Event;
 using UnityEngine;
 using Utils;
@@ -7,18 +8,32 @@ namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private GameEvent IngressCompleted;
-        
         [SerializeField] private float ConstraintX;
         [SerializeField] private float ConstraintY;
         [SerializeField] private float Speed;
-        [SerializeField] private Vector2 _startPos;
+        [SerializeField] private float IngressDuration;
+        [SerializeField] private Ease IngressEase;
+        [SerializeField] private Vector2 StartPos;
         
         private bool _horizontalConstraint;
         private bool _verticalConstraint;
+        private bool _ingressCompleted;
+
+        private void Awake()
+        {
+            transform.position = StartPos;
+        }
+
+        private void Start()
+        {
+            transform.DOMoveY(0, IngressDuration).SetEase(IngressEase)
+                .OnComplete(() => _ingressCompleted = true);
+        }
 
         private void Update()
         {
+            if(!_ingressCompleted)
+                return;
             var position = transform.position;
             _horizontalConstraint = position.x < ConstraintX && position.x > -ConstraintX;
             _verticalConstraint = position.y < ConstraintY && position.y > -ConstraintY;
